5000ce061b1cdfda3ef23d870640223005799d45
Andreas Galanomatis <andreas@cloud66.com>
2016-01-21 10:37:42 +0000

    Docker

---
+++ source.sh
@@ -0,0 +1,4 @@
+export RAILS_ENV=development
+export MYSQL_USERNAME=root
+export MYSQL_ADDRESS=localhost
+export MYSQL_DATABASE=rails-mysql
---
+++ start.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+bundle exec rake db:create
+bundle exec rake db:schema:load
+bundle exec rake db:migrate
+
+bundle exec rails server -b 0.0.0.0
